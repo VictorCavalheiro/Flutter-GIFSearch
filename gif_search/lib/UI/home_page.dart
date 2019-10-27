e56c354310f49b9abb11d76f0b518d0063b883c1
victor cavalheiro <victor.cavalheiro17@gmail.com>
2019-10-27 12:22:42 -0300

    adding grid images and load more

--- gif_search/lib/UI/home_page.dart
+++ gif_search/lib/UI/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,7 +14,10 @@ class _HomePageState extends State<HomePage> {
   static String trendingURL =
       "https://api.giphy.com/v1/gifs/trending?api_key=rnTU9iVdd3ChegLQiMCMRpYU7DSzWnoS&limit=20&rating=G";
   static String searchURL =
-      "https://api.giphy.com/v1/gifs/search?api_key=rnTU9iVdd3ChegLQiMCMRpYU7DSzWnoS&q=$_searchString&limit=20&offset=$_offset&rating=G&lang=en";
+      "https://api.giphy.com/v1/gifs/search?api_key=rnTU9iVdd3ChegLQiMCMRpYU7DSzWnoS&q=$_searchString&limit=19&offset=$_offset&rating=G&lang=en";
+
+  Map<String, dynamic> dataFromJson = new Map();
+
 
   Future<Map> _getGIFS() async {
     http.Response response;
@@ -23,7 +26,7 @@ class _HomePageState extends State<HomePage> {
     } else {
       response = await http.get(searchURL);
     }
-
+    print(response);
     return json.decode(response.body);
   }
 
@@ -33,12 +36,47 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     _getGIFS().then((value) {
-      print(value);
+      dataFromJson = value;
     });
   }
 
-  Widget _createGifTable(context,snapshot){
-    return Container();
+  int _getCountGifs(List list) {
+    if (_searchString == null) {
+      return list.length;
+    }
+    else {
+      return list.length + 1;
+    }
+  }
+
+  Widget _createGifTable(context, snapshot) {
+
+    return GridView.builder(
+        padding: EdgeInsets.all(5.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 5.0, mainAxisSpacing: 5.0),
+        itemCount: _getCountGifs(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_searchString == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+
+                child: Image.network(
+                    snapshot
+                        .data["data"][index]["images"]["fixed_height"]["url"],
+                    height: 300.0,
+                    fit: BoxFit.cover),
+                );
+          } else {
+            return Container(child: GestureDetector(child: Column(
+              mainAxisAlignment:MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 50.0),
+                  Text("Load more",style: TextStyle(color: Colors.white,fontSize: 20.0))
+                ]),onTap: (){setState(() {
+                  _offset+=19;
+                });}));
+          }
+        });
   }
 
   @override
@@ -51,42 +89,47 @@ class _HomePageState extends State<HomePage> {
                 "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif")),
         backgroundColor: Colors.black,
         body: Column(children: <Widget>[
-          Padding(padding: EdgeInsets.all(5.0), child: TextField(
-              decoration: InputDecoration(
-                  labelText: "Search GIF",
-                  labelStyle: TextStyle(color: Colors.white),
-                  border: OutlineInputBorder()),
-              style: TextStyle(color: Colors.white, fontSize: 18.0),
-              textAlign: TextAlign.center)),
-          Expanded(child: FutureBuilder(future: _getGIFS(),
-              builder: (context, snapshot) {
+          Padding(
+              padding: EdgeInsets.all(5.0),
+              child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Search GIF",
+                      labelStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder()),
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  textAlign: TextAlign.center,
+                  onSubmitted: (value) {
+                    setState(() {
+                      _searchString = value;
+                    });
+                  })),
+          Expanded(
+              child: FutureBuilder(
+                  future: _getGIFS(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case (ConnectionState.none):
+                      case (ConnectionState.waiting):
+                        return Container(
+                            height: 200.0,
+                            width: 200.0,
+                            alignment: Alignment.center,
+                            child: CircularProgressIndicator(
+                              valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                              strokeWidth: 4.0,
+                            ));
 
-                switch(snapshot.connectionState){
-                  case(ConnectionState.none):
-                  case(ConnectionState.waiting):
-                    return Container(height: 200.0,
-                        width: 200.0,
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),strokeWidth: 4.0,
-                        ));
-
-                  default:
-                    if(snapshot.hasError){
-                      return Container(child: Text("Erro encontrado",style: TextStyle(color:Colors.white)));
+                      default:
+                        if (snapshot.hasError) {
+                          return Container(
+                              child: Text("Erro encontrado",
+                                  style: TextStyle(color: Colors.white)));
+                        } else {
+                          return _createGifTable(context, snapshot);
+                        }
                     }
-                    else{
-                      return Container();
-                      //_createGifTable(context,snapshot);
-                    }
-                }
-
-              }
-
-
-
-          ))
-
+                  }))
         ]));
   }
 }
